be4ac206670df77ef6e63a664e5b569a3b0d5e32
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-06 18:14:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import socket
 import os
 
-st.write(os.environ.get("STREAMLIT_HOST", ""))
+if os.getenv("HOSTNAME") == "streamlit":
+  st.write("welcome to the cloud")
+
+st.write(os.environ)
