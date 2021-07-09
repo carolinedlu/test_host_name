3829104c059b68bd882f516b41cce7600e43c580
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-09 14:33:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import os
+from streamlit import caching
 
 if os.getenv("HOSTNAME") == "streamlit":
   st.write("welcome to the cloud")
 
 st.write(os.environ)
+
+
+
+caching.clear_cache()
